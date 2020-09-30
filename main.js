1393c9edf2e5ffd221306cf1507f23b006b27dca
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-09-30 09:58:07 -0400

    progress

--- main.js
+++ main.js
@@ -1,23 +1,64 @@
-//1:
-//Write a function compareToTen that takes a number, num as an argument and returns a Promise that tests if the value of num is less than or greater than the value 10.
-// if number is greater or equal it should resolve  with a string and log for example, if num = 20 it should say '20 is greater than or equal to 10, resolved!'
-// if num is less than 10 then it should reject and log for example '5 is less than 10, error!'
-// create a variable randomNum that creates a random number between 1 and 20
-// use it as your argument to invoke your compareToTen function
+/*1:
+Write a function compareToTen that takes a number, num as an argument and returns a Promise 
+that tests if the value of num is less than or greater than the value 10.
+if number is greater or equal it should resolve  with a string and log for example, 
+if num = 20 it should say '20 is greater than or equal to 10, resolved!'
+if num is less than 10 then it should reject and log for example '5 is less than 10, error!'
+create a variable randomNum that creates a random number between 1 and 20
+use it as your argument to invoke your compareToTen function */
 
-//2.
-// create a global variable called order. It will hold a string with whatever drink you want to order, for example, a 'Slurpy'
-//create a promise called drink
-// inside your promise:
-// first log 'I'll be right back with your <order variable>'.
-// next create another boolean variable, orderCannotBeFilled. Set it to false
-// when order is orderCannotBeFilled is true, promise should reject after 2 seconds with a message,
-// 'Sorry we are all out of <whatever the order is>' (not hard coded order)
-// otherwise resolve the order after 4 seconds saying 'Server returns: 'Here is your <whatever the order is>'
+let num = Math.floor(Math.random() * 20); //?
 
+let compareToTen = new Promise((resolve, reject) => {
+  if (num >= 10) {
+    resolve(`${num} is greater than or equal to 10, resolved!`);
+  } else {
+    reject(`${num} is less than 10, error!`);
+  }
+});
+
+compareToTen.then((message) => {
+  console.log(message);
+}).catch((message) => {
+  console.log(message);
+});
+
+
+/*2.
+create a global variable called order. It will hold a string with whatever drink you want to order, for example, a 'Slurpy'
+create a promise called drink
+inside your promise:
+first log 'I'll be right back with your <order variable>'.
+next create another boolean variable, orderCannotBeFilled. Set it to false
+when order is orderCannotBeFilled is true, promise should reject after 2 seconds with a message,
+'Sorry we are all out of <whatever the order is>' (not hard coded order)
+otherwise resolve the order after 4 seconds saying 'Server returns: 'Here is your <whatever the order is>' */
+
+let order = 'slurpy';
+
+
+let drink = new Promise((resolve, reject) => {
+  console.log(`I'll be right back with you ${order}.`);
+  let orderCannotBeFilled = false;
+  if (orderCannotBeFilled) {
+    setTimeout(() => {
+      reject(`Sorry we are all out of ${order}.`);
+    }, 2000);
+  } else {
+    setTimeout(() => {
+      resolve(`Server returns: 'Here is your ${order}'`);
+    }, 4000);
+  }
+}); 
 // Now consume the promise
 // When the order resolves, it should log 'Server Returns:' and the resolve message.
 // Handle the catch. It should log 'Sorry we are all out of <whatever the order is>'
+
+drink.then((message) => {
+  console.log(message);
+}).catch((message) => {
+  console.log(message);
+})
 
 // Test by changing the value of orderCannotBeFilled between true and false
 
@@ -34,24 +75,52 @@
 //3.CHAIN
 //Write two separate functions that return promises
 // The first function, makeAllCaps(), will take in an array of words and capitalize them.
-//the second function, sortWords(), will sort the words in alphabetical order.
-//If the array contains anything but strings, it should throw an error.
+// the second function, sortWords(), will sort the words in alphabetical order.
+// If the array contains anything but strings, it should throw an error.
 // Test the functions separately
 // Then test what happens if you chain the 2 functions on a successful array
 
 const arrayOfWords = ['cucumber', 'tomatos', 'avocado']; //returns ['CUCUMBER','TOMATOES','AVOCADO']
 const complicatedArray = ['cucumber', 44, true]; //returns "Error Not All Items are strings"
 
-//4.
-// a. Create a variable, totalSales that creates a promise.
-// b. Within the promise create a reject handler for errors and a resolve handler that resolves a copy of the array that is inside the json object.
-// c. Call or rather consume your totalSales promise
-// d. Use your thenable to log the data for all the companies in the Legal industry
-// e. pass the data for all the companies in the Legal industry to the next thenable
-// f. Chain again and find all the names of the people in the legal industry and log those names in a string: "Contacts from the legal profession:" + list all of the names
-// g. In that same thenable return 30% of each of the sales and pass the value to the next thenable
-// h. Add all of the sales totals together and return a string saying: "Total sales for everyone from the Legal profession after taxes is $30000" if the value of the added sales is 30000
-const json = {
+let arr = complicatedArray;
+
+let words = new Promise((resolve, reject) => {
+  results = [];
+  arr.map(item => {
+    typeof item === "string" ? results.push(true)
+                            : results.push(false)
+    } 
+);
+  results.includes(false) ? reject("Error: Not All Items are strings.")
+                          : resolve(arr.map(item => item.toUpperCase()))
+  })
+
+words.then((allCaps) => {
+  console.log(allCaps.sort()); //?
+}).catch((err) => {
+  (console.log(err)); //?
+})
+
+
+
+/*4.
+  a. Create a variable, totalSales that creates a promise.
+  b. Within the promise create a reject handler for errors and a resolve handler that resolves a 
+  copy of the array that is inside the json object.
+  c. Call or rather consume your totalSales promise
+  d. Use your thenable to log the data for all the companies in the Legal industry
+  e. pass the data for all the companies in the Legal industry to the next thenable
+  f. Chain again and find all the names of the people in the legal industry and log those names in a 
+  string: "Contacts from the legal profession:" + list all of the names
+  g. In that same thenable return 30% of each of the sales and pass the value to the next thenable
+  h. Add all of the sales totals together and return a string saying: "Total sales for everyone from 
+  the Legal profession after taxes is $30000" if the value of the added sales is 30000 */
+
+
+
+
+  const json = {
   data: [
     {
       name: 'NextGen Advisors',
@@ -110,24 +179,56 @@ const json = {
   ],
 };
 
-// 5. Based on given athlets array
-//a.  Write a function called playerFunction that returns a promise which copies the array into a new array called 'playerArr'.
-// Make sure your function is re-useable so no hard coded values.
-//b. Now invoke the function
-// loop through the function using array method and Place a key value pair into each object based on the player's position.
-//If the position is guard then add sport:'basketball'
-// If the position is quarterback add sport:'football'
-// Use a ternary to solve this.
-// Note: There can only be two sports either football or basketball and only two positions guard and quarterback
-//c.
-// Log (console.log) the whole array with the sport added for each object. Preface the list with a 'Sport added: ' string.
-// console.log('---------') to separate a section.
-//d.
-// In your next thenable, Console.log a string like the example below for each player:
-// 'Tom Bradshaw plays football and is a quarterback for the Pittsburgh Steelers'
-// Only choose the players who play football and use deconstruction and the ternary operator to complete this piece
-//e. In the next thenable, console.log the original array to show it has not been mutated
-//f. Be sure to include you Promise Error Handling using the catch
+let totalSales = new Promise((resolve, reject) => {
+  resolve(json);
+  reject("Error");
+})
+
+totalSales.then((dataArr) => {
+  const {data} = dataArr;
+  return data.filter(item => {
+    if (item.industry === 'Legal') {
+      return item;
+  }})
+}).then((info) => {
+  console.log(info);
+  return info;
+}).then((allLegal) => {
+  console.log('Legal Team Contacts:');
+  allLegal.forEach(person => console.log(`${person.contact}`));
+  return allLegal.map(sale => (sale.sales) * .3); //?
+}).then((bigSales) => {
+  let answer = bigSales.reduce((total, current) => total + current, 0); //?
+  console.log(`Total sales for everyone from the Legal profession after taxes is $${answer}.`)
+})
+.catch((errMessage) => {
+  console.log(errMessage);
+})
+
+/* 5. Based on given athlets array
+  a.  Write a function called playerFunction that returns a promise which copies the array into a 
+      new array called 'playerArr'.
+      Make sure your function is re-useable so no hard coded values.
+  b. Now invoke the function
+      loop through the function using array method and Place a key value pair into each object based 
+      on the player's position.
+      If the position is guard then add sport:'basketball'
+      If the position is quarterback add sport:'football'
+      Use a ternary to solve this.
+      Note: There can only be two sports either football or basketball and only two positions guard 
+      and quarterback
+  c.
+      Log (console.log) the whole array with the sport added for each object. Preface the list with 
+      a 'Sport added: ' string.
+      console.log('---------') to separate a section.
+  d.
+      In your next thenable, Console.log a string like the example below for each player:
+      'Tom Bradshaw plays football and is a quarterback for the Pittsburgh Steelers'
+      Only choose the players who play football and use deconstruction and the ternary operator to 
+      complete this piece
+  e. In the next thenable, console.log the original array to show it has not been mutated
+  f. Be sure to include you Promise Error Handling using the catch */
+
 let athletes = [
   {
     name: 'Tom Brady',
